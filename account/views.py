a2bf2fde1e9beef36114d1a745beb16edd1b621f
DESKTOP-T1FEPOR\Filip <filip.wiatrowski@gmail.com>
2021-01-31 21:11:12 +0100

    I have added the built-in Django authentication log in and log out views to your project I have created custom templates for both views and defined a simple dashboard view to redirect users after they log in Configured your settings for Django to use these URLs by default

--- account/views.py
+++ account/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def user_login(request):
+    '''
+    User login view. If request is GET render login form. When request is POST instantiate form, check if data is valid
+    authenticate user against database (authenticate() method), check if user is active (is_active attribute from user
+    model) and at the end return login.html with positive message.
+    :param request:
+    :return:
+    '''
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            # check user credentials against database and return User object or None
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user) # sets user in current session
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form':form})
+
+
+@login_required
+def dashboard(request):
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
